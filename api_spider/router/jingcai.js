0bcec0ef8f0697078b0f204adcdb628cdadcab2f
xiaogang <wxungang@163.com>
2018-04-20 18:58:14 +0800

    init two

--- api_spider/router/jingcai.js
+++ api_spider/router/jingcai.js
@@ -29,14 +29,23 @@ router.get('/', function (req, res, next) {
 
 
 /**
+ * 百家奖金（以下数据仅供参考）
  * url: http://i.sporttery.cn/api/fb_match_info/get_europe/?f_callback=hb_odds&dc_mid=616474
  */
-router.post('/sporttery', function (req, res, next) {
+router.post('/bjj', function (req, res, next) {
+    let _url = 'http://i.sporttery.cn/api/fb_match_info/get_europe/?f_callback=hb_odds&dc_mid=';
     let _result = null;
     let _body = req.body;
     console.log(req.body);
     console.log(req.session);
     let _cmd = req.originalUrl || req.baseUrl + req.url;
+
+    //url
+    if (_body.url) {
+        _url = _body.url;
+    }
+    _url += _body.mid || 616474;
+    _body.url = _url;
 
     //返回异常处理结果
     if (_result) {
@@ -52,5 +61,37 @@ router.post('/sporttery', function (req, res, next) {
     )
 });
 
+/**
+ * 竞彩胜平负
+ * url: http://i.sporttery.cn/api/fb_match_info/get_odds/?mid=106925&f_callback=sportteryOdds
+ * 从页面 dom 中抓取 数据
+ */
+router.post('/jps', function (req, res, next) {
+    let _url = 'http://i.sporttery.cn/api/fb_match_info/get_odds/?f_callback=sportteryOdds&mid=';
+    let _result = null;
+    let _body = req.body;
+    console.log(req.body);
+    console.log(req.session);
+    let _cmd = req.originalUrl || req.baseUrl + req.url;
+
+    //url
+    if (_body.url) {
+        _url = _body.url;
+    }
+    _url += _body.mid || 106925;
+    _body.url = _url;
+
+
+    //返回异常处理结果
+    if (_result) {
+        res.json(Object.assign(_result, {cmd: _cmd}));
+        return;
+    }
+    //service
+    jingcai_service.sporttery(_body).then(
+        data => res.json(res_format.response_format({result: data, cmd: _cmd})),
+        err => res.json(res_format.response_without_result({result: err, cmd: _cmd}))
+    )
+});
 
 module.exports = router;
--- api_spider/service/jingcai.js
+++ api_spider/service/jingcai.js
@@ -8,20 +8,26 @@
 //mysql
 // var user_pool = require("../mysql/mysql_pool_user");
 
+const iconv = require('iconv-lite');
+const cheerio = require('cheerio');
 const request = require('request');
 //util
 const res_format = require("../util/response_format");
 const util = require("../util/util");
 /**
- * 用户注册
+ *  http://info.sporttery.cn/football/info/fb_match_hhad.php?m=106925
+ *  竞猜通用 请求
  * @param params
  * @param callback
  */
 exports.sporttery = function (params) {
     return new Promise((resolve, reject) => {
+        if (!params.url) {
+            reject(params);
+        }
         request({
-            uri: 'http://i.sporttery.cn/api/fb_match_info/get_europe/?f_callback=hb_odds&dc_mid=616474',
-            method: "get",
+            uri: params.url,//'http://i.sporttery.cn/api/fb_match_info/get_europe/?f_callback=hb_odds&dc_mid=616474',
+            method: params.method || "get",
             gzip: true,
             // encoding: null
         }, function (err, res, resBody) {
@@ -38,16 +44,33 @@ exports.sporttery = function (params) {
                 }
 
             }
-            console.log(err);
-            // console.log("=============== proxy _resBody 3 ==========");
-            // console.log(iconv.decode(_resBody, 'gb2312'));
-            // console.log("=============== proxy _res 3 ==========");
-            // console.log(res);
-            // console.log(resBody);
-            //返回的body 直接就是buffer 了...
+        });
 
-            // const $ = cheerio.load(iconv.decode(_resBody, 'gb2312'));
-            // console.log($('#bj_table').html());
+    });
+
+};
+/**
+ * dom 中爬取 相关 数据
+ * @param params
+ * @returns {Promise}
+ */
+exports.sportPage = function (params) {
+    return new Promise((resolve, reject) => {
+        request({
+            uri: params.url,
+            method: "get",
+            gzip: true,
+            encoding: null
+        }, function (err, res, resBody) {
+            if (err) {
+                reject(err);
+            } else {
+                //返回的body 直接就是buffer 了...
+                const $ = cheerio.load(iconv.decode(resBody || res.body, 'gb2312'));
+                console.log($('#Top').html());
+                resolve($('#Top').html());
+            }
+            console.log(err);
 
         });
 
